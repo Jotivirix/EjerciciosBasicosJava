fe19679a91cdd08dc5139af48e07c6c4960b3b19
Jotivirix <Jotivirix@MacBook-Pro-de-Jose.local>
2015-11-16 20:13:42 +0100

    Terminados Ejercicios 1,2,3

--- src/EjerciciosRepaso.java
+++ src/EjerciciosRepaso.java
@@ -6,6 +6,16 @@ public class EjerciciosRepaso extends ConsoleProgram {
 		println(fiestaArdillas(30,false));
 		println(fiestaArdillas(50,false));
 		println(fiestaArdillas(70,true));
+		println(multa(60,false));
+		println(multa(65,false));
+		println(multa(65,true));
+		println(multa(66,true));
+		println(multa(98,false));
+		println(muyVanidoso(22));
+		println(muyVanidoso(23));
+		println(muyVanidoso(24));
+		println(muyVanidoso(26));
+		println(muyVanidoso(69));
 	}
 
 	public boolean fiestaArdillas(int numeroBellotas, boolean festivo){
@@ -20,7 +30,30 @@ public class EjerciciosRepaso extends ConsoleProgram {
 			return true;
 		}
 		return false;
-
-
+	}
+	
+	public int multa (int velocidad, boolean cumple){
+		if(cumple){
+			velocidad = velocidad-5;
+		}
+		if(velocidad <=60){
+			return 0;
+		}
+		else if (velocidad >60 && velocidad <=80){
+			return 1;
+		}
+		return 2;
+	}
+	
+	public boolean muyVanidoso (int numero){
+		if(numero%11==0){
+			return true;
+		}
+		else if(numero%11==1){
+			return true;
+		}
+		return false;
+		
+		
 	}
 }
